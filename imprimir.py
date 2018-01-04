5e3c82014a3bac0a7a250f1ce2570d770c59fcb5
LanzMarz <lanz_marz@outlook.es>
2018-01-04 16:30:42 -0400

    Capítulo Introductorio

---
+++ imprimir.py
@@ -0,0 +1,12 @@
+#Imprimir en pantalla con la función Print()
+#Con comillas dobles
+print("Python es un lenguaje de programación multiplataforma")
+
+#Con Comillas simples
+print('Python es un lenguaje de programación multiplataforma')
+
+#Imprimir Comillas - Primera Opción
+print("En Python no te debes 'preocupar' del ;")
+
+#Imprimir Comillas - Segunda Opción 
+print('En Python no te debes "preocupar" del ;')
---
+++ variables.py
@@ -0,0 +1,36 @@
+#Variables en Python
+'''
+#Nombrar a las variables en relación a su contenido
+
+salario = 2000
+interes = 0.5
+usuario = "Condorito"
+
+print(usuario)
+
+'''
+
+#Operar con Variables - caso signo +
+
+#nombre1 = 'Pepito'
+#nombre2 = 'Juanita'
+#nombre3 = 'Jaime'
+#gestion = 2018 
+
+#print("Erase una vez, un estudiante llamado"+nombre1+", su novia se llamaba"+nombre2+"y su padre el Dr."+nombre3+"que era el Rector de la Universidad.")
+
+#Operar con Variables - caso signo ,
+
+#print("Erase una vez, un estudiante llamado",nombre1,",su novia se llamaba",nombre2,"y su padre el Dr.",nombre3,"que era el Rector de la Universidad.")
+
+#Optimizando la operacionalización - cadenas de texto y números
+
+#print("Erase una vez, un estudiante llamado %s ,su novia se llamaba %s y su padre el Dr. %s que era el Rector de la Universidad desde %d." %(nombre1, nombre2, nombre3, gestion))
+
+libro1 = 'Rebelión en la Granja'
+libro2 = 'El Señor de las Moscas'
+publi1 = 1945
+publi2 = 1954
+
+
+print("Libros de Interesantes de George Orwell:\n %s\t %d \n Libros Interesantes de William Golding:\n %s\t %d"%(libro1, publi1, libro2, publi2))
